d521c290e727a2c0ee120a07d564202dcaca5553
raisasharom <64147148+raisasharom@users.noreply.github.com>
2023-11-02 13:53:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit 
 
+streamlit.title('my Parents new Healthy Diner')
+Streamlit.header('Breakfast Menu')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard Boiled egg free range egg')
